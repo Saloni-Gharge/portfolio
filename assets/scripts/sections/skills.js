21ebc4bdc5268c72ce7a7f6d857b41199d0d7a03
Saloni-Gharge <ghargesau@gmail.com>
2025-03-01 02:05:29 -0800

    Create skills.js

---
+++ assets/scripts/sections/skills.js
@@ -0,0 +1,18 @@
+import Filterizr from 'filterizr'
+
+document.addEventListener('DOMContentLoaded', () => {
+  // ================== Skill cards =====================
+
+  // setup skill filter buttons
+  const skillCardHolder = document.getElementById('skill-card-holder')
+  if (skillCardHolder != null && skillCardHolder.children.length !== 0) {
+    // Initialize Filterizr
+    const filterizr = new Filterizr('.filtr-skills', {
+      layout: 'sameWidth',
+      controlsSelector: '.skill-filtr-control'
+    });
+
+    // Set default filter to "programming"
+    filterizr.filter('programming');
+  }
+});
